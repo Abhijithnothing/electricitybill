3735da875f1c8241c5bf796ffb06d8461151a8cb
Abhijithnothing <a00931668@gmail.com>
2025-10-04 11:31:55 +0530

    discount

--- electricity.py
+++ electricity.py
@@ -1,3 +1,8 @@
-unit=int(intput("Enter the units comsumed:))
+unit=int(input("Enter the units comsumed:"))
 r=5*unit
 print("Rs comsumed on units is :",r)
+if r>1000:
+    r=r-(r*0.1)
+    print("Rs after 10% discount is :",r)
+else:
+    print("No discount")
